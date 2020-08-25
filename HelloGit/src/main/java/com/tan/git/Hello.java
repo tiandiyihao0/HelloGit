e837992b72977a348d327975e670895dda97306a
tiandiyihao0 <41739107+tiandiyihao0@users.noreply.github.com>
2020-08-25 14:42:30 +0800

    在线更新

--- HelloGit/src/main/java/com/tan/git/Hello.java
+++ HelloGit/src/main/java/com/tan/git/Hello.java
@@ -16,5 +16,6 @@ public class Hello {
         System.out.println("创建dev分支");
         System.out.println("master添加");
         System.out.println("dev添加");
+        System.out.println("在线更新");
     }
 }
